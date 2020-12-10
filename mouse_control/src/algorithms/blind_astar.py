f66995aa5dfa1a23472f87b47e27274512a38559
al-exe <al.exe@berkeley.edu>
2020-12-10 04:29:51 -0800

    lvl 2 in progress

--- mouse_control/src/algorithms/blind_astar.py
+++ mouse_control/src/algorithms/blind_astar.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import rospy
+import numpy as np
 
 from mouse_description.msg import MouseCommand
 from . import path_finding
@@ -29,6 +30,7 @@ def computeFlags(rmap):
 					myFlag = (x,y)
 				else:
 					enemyFlag = (x,y)
+				reconMap[x][y] = 'F'
 	for x in range(WORLD_WIDTH):
 		for y in range(WORLD_HEIGHT // 2, WORLD_HEIGHT):
 			if rmap[x][y] == 'F':
@@ -36,12 +38,29 @@ def computeFlags(rmap):
 					enemyFlag = (x,y)
 				else:
 					myFlag = (x,y)
+				reconMap[x][y] = 'F'
+
 
 # standard interface functions
 def initAlg(isant, numMice):
-	global ISANT, NUM
+	global ISANT, NUM, lastxs, lastys, currentxs, currentys
 	ISANT = isant
 	NUM = numMice
+
+	#listxs = [-1 for a in range(NUM)]
+	#listys = [-1 for b in range(NUM)]
+	#listxs = np.zeros(NUM)
+	#listys = np.zeros(NUM)
+	lastxs = []
+	lastxs = [-1 for _ in range(NUM)]
+	lastys = []
+	lastys = [-1 for _ in range(NUM)] 
+
+	currentxs = []
+	currentxs = [-1 for _ in range(NUM)]
+	currentys = []
+	currentys = [-1 for _ in range(NUM)]
+
 
 def computeMoves(miceMoves, score, miceData, omniMap):
 	# miceMoves - modify this with the moves u wanna do
@@ -69,21 +88,43 @@ def computeMoves(miceMoves, score, miceData, omniMap):
 			types = current_mouse.types
 			xs = current_mouse.xs
 			ys = current_mouse.ys
+	
+			# init current x and y
+			currentxs[i] = mx
+			currentys[i] = my
 
 			# created an updated recon map with radius information here:
 			updatedReconMap = copy.deepcopy(reconMap)
 
-			for i in range(len(types)):
-				newx, newy = xs[i], ys[i]
-				updatedReconMap[newx][newy] = types[i]
+			# update map with all vision
+			for j in range(len(types)):
+				newx, newy = xs[j], ys[j]	
+				updatedReconMap[newx][newy] = types[j]
+
+			# delete old one
+			#print(lastxs[i])
+			#print(currentxs[i])
+			#print(lastys[i])
+			#print(currentys[i])
+			if lastxs[i] != -1 and lastys[i] != -1:
+				if currentxs[i] != lastxs[i] or currentys[i] != lastys[i]:
+					print("HIT")
+					updatedReconMap[lastxs[i]][lastys[i]] = ' '
+
+			lastxs[i] = currentxs[i]
+			lastys[i] = currentys[i]
 
 			reconMap = updatedReconMap
 
+			print(updatedReconMap)
+
 			if 'AF' in reconMap[mx][my]:
 				# HAS FLAG
+				print("ANT GOING HOME!")
 				close_list = path_finding.astar(mx, my, myFlag[0], myFlag[1], reconMap, WORLD_HEIGHT, WORLD_WIDTH)
 			else:
 				# DOES NOT HAVE FLAG
+				print("ANT GOING IN!")
 				close_list = path_finding.astar(mx, my, enemyFlag[0], enemyFlag[1], reconMap, WORLD_HEIGHT, WORLD_WIDTH)
 						
 			nextx, nexty = 0, 0
@@ -127,20 +168,41 @@ def computeMoves(miceMoves, score, miceData, omniMap):
 			xs = current_mouse.xs
 			ys = current_mouse.ys
 			
+			# init current x and y
+			currentxs[i] = mx
+			currentys[i] = my
+
 			# created an updated recon map with radius information here:
 			updatedReconMap = copy.deepcopy(reconMap)
 
-			for i in range(len(types)):
-				newx, newy = xs[i], ys[i]
-				updatedReconMap[newx][newy] = types[i]
+			# update map with all vision
+			for j in range(len(types)):
+				newx, newy = xs[j], ys[j]	
+				updatedReconMap[newx][newy] = types[j]
 
-			reconMap = updatedReconMap		
+			# delete old one
+			#print(lastxs[i])
+			#print(currentxs[i])
+			#print(lastys[i])
+			#print(currentys[i])
+			if lastxs[i] != -1 and lastys[i] != -1:
+				if currentxs[i] != lastxs[i] or currentys[i] != lastys[i]:
+					updatedReconMap[lastxs[i]][lastys[i]] = ' '
+
+			lastxs[i] = currentxs[i]
+			lastys[i] = currentys[i]
+
+			reconMap = updatedReconMap
+
+			print(updatedReconMap)
 
 			if 'BF' in reconMap[mx][my]:
 				# HAS FLAG
+				print("BEE GOING HOME!")
 				close_list = path_finding.astar(mx, my, myFlag[0], myFlag[1], reconMap, WORLD_HEIGHT, WORLD_WIDTH)
 			else:
 				# DOES NOT HAVE FLAG
+				print("BEE GOING IN!")
 				close_list = path_finding.astar(mx, my, enemyFlag[0], enemyFlag[1], reconMap, WORLD_HEIGHT, WORLD_WIDTH)
 							
 			nextx, nexty = 0, 0
@@ -175,4 +237,4 @@ def computeMoves(miceMoves, score, miceData, omniMap):
 				else:
 					miceMoves[i].type = MouseCommand.LEFT
 
-	reconMap = updatedReconMap
+	#reconMap = updatedReconMap
